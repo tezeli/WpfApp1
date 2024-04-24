42904fb03d352030a263c0591ffa17413d53e941
227364 <227364@KB37-1-115-C13>
2024-04-24 11:21:58 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/AddData.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для AddData.xaml
+    /// </summary>
+    public partial class AddData : Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        SqlDataAdapter adapter;
+        DataTable AeroportTable;
+        SqlConnection connection = null;
+        public AddData()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (airname_box.Text != "" && date_box.Text != "" && capacity_box.Text != "" && time_box.Text != "" && TO_box.Text != "")
+            {
+                string sql = $"insert into Airplanes values ('{airname_box.Text}','{date_box.Text}',{capacity_box.Text},'{time_box.Text}',{TO_box.Text})";
+                AeroportTable = new DataTable();
+
+                //try
+                //{
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.ExecuteNonQuery();
+                adapter = new SqlDataAdapter(command);
+                MessageBox.Show("Данные добавлены");
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+                //}
+                //finally
+                //{
+
+                if (connection != null)
+                    connection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Не все поля были заполнены");
+            }
+        }
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(int x)
+        {
+            InitializeComponent();
+            switch (x)
+            {
+                case 1:
+                    AddData addData = new AddData();
+                    MainFrame.Content = addData;
+                    break;
+            }
+        }
+    }
+}
